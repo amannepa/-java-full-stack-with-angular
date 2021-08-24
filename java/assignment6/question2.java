e5a8f737eca9e50916f1a44350abfc78dd50c92e
amannepa <88093861+amannepa@users.noreply.github.com>
2021-08-24 17:22:51 +0530

    Add files via upload

---
+++ java/assignment6/question2.java
@@ -0,0 +1,24 @@
+package collection;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Set<String> product=new TreeSet<>();
+		product.add("ammu");
+		product.add("chenna");
+		product.add("kesava");
+		product.add("keerthi");
+		product.add("madhu");
+		product.add("sree");
+		
+		product.add("ammu");
+		System.out.println(product);
+				
+
+	}
+
+}
