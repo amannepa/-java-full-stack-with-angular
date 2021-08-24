f90932df33417cc26ee09cf8f9d10ea03e695ce1
amannepa <88093861+amannepa@users.noreply.github.com>
2021-08-24 17:19:01 +0530

    Add files via upload

---
+++ java/assignment2/question3.java
@@ -0,0 +1,40 @@
+package javaoops;
+
+import java.util.Scanner;
+
+class bank{
+	public int amount(int amt) {
+		return 0;
+	}
+}
+class Savings extends bank{
+	int amt;
+	
+	public int amount(int amt){
+		
+		this.amt=amt;
+		return amt;
+	}
+}
+class Current extends bank{
+	int amt;
+	public int amount(int amt) {
+		this.amt=amt;
+		return amt;
+	}
+}
+
+public class question3{
+  public static void main(String args[]){
+	  Savings s=new Savings();
+	  Current c=new Current();
+	  
+	  Scanner sc=new Scanner(System.in);
+	  int savings=sc.nextInt();
+	  int current=sc.nextInt();
+	  
+	  int sum=s.amount(savings)+c.amount(current);
+
+	  System.out.println("Total amount of cash in the bank is :"+sum);
+  }
+}
