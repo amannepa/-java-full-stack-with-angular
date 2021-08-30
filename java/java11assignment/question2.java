8fb4991e095974941d9a34e38c5f4a7d7f38f083
amannepa <88093861+amannepa@users.noreply.github.com>
2021-08-30 22:18:43 +0530

    Add files via upload

---
+++ java/java11assignment/question2.java
@@ -0,0 +1,24 @@
+package java11;
+
+import java.util.*; 
+
+interface myInt { 
+    
+    int add(int a, int b); 
+} 
+class question2 { 
+    public static void main(String[] args) 
+    { 
+    	
+      System.out.println("1.var cannot be used with generic type");
+          
+        
+      System.out.println("2.var cannot be used with Lambda Expression");
+      
+      System.out.println("3.var cannot be used in global variable declaration");
+      
+      System.out.println("4.var cannot be used without explicit initialization");
+      
+      
+    } 
+}
